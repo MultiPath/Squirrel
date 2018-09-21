b37f6bcce178618ba9df1a2fbd81b8dff11794dc
Jiatao Gu <jgu@fb.com>
2018-09-21 01:17:49 -0400

    makes the code clearner

--- model_c.py
+++ model_c.py
@@ -10,6 +10,7 @@ import torch
 import math
 
 from collections import defaultdict
+from namedlist import namedlist
 from abc import ABCMeta, abstractmethod
 from torch import nn
 from torch.nn import functional as F
@@ -18,6 +19,41 @@ from model import *
 
 INF = 1e10
 TINY = 1e-9
+
+class Message(namedlist('Message', ['data1d', 'mask1d', 'data2d', 'mask2d',
+                                    'idx1d', 'idx2d', 'batch_size', 
+                                    'max_num_char', 'max_size_word', 'max_num_word',
+                                    'causal_mask'])):
+    def to2d_(self):
+        
+        if len(self.data1d.size()) == 3:  # include model dimension
+            d_model = self.data1d.size(-1)
+            self.data2d = self.data1d.new_zeros(self.batch_size * self.max_num_word * self.max_size_word, d_model)
+            # self.data2d.resize_(self.batch_size * self.max_num_word * self.max_size_word, d_model).zero_()
+            self.data2d[self.idx2d] = self.data1d.view(-1, d_model)[self.idx1d]
+            self.data2d = self.data2d.view(self.batch_size, self.max_num_word, self.max_size_word, d_model)
+        else:
+            self.data2d = self.data1d.new_zeros(self.batch_size * self.max_num_word * self.max_size_word)
+            self.data2d[self.idx2d] = self.data1d.view(-1)[self.idx1d]
+            self.data2d = self.data2d.view(self.batch_size, self.max_num_word, self.max_size_word)
+
+    def to1d_(self):
+
+        if len(self.data2d.size()) == 4:  # include model dimension
+            d_model = self.data2d.size(-1)
+            self.data1d = self.data2d.new_zeros(self.batch_size * self.max_num_char, d_model)
+            # self.data1d.resize_(self.batch_size * self.max_num_char, d_model).zero_()
+            self.data1d[self.idx1d] = self.data2d.view(-1, d_model)[self.idx2d]
+            self.data1d = self.data1d.view(self.batch_size, self.max_num_char, d_model)
+
+        else:
+            self.data1d = self.data2d.new_zeros(self.batch_size * self.max_num_char)
+            self.data1d[self.idx1d] = self.data2d.view(-1)[self.idx2d]
+            self.data1d = self.data1d.view(self.batch_size, self.max_num_char)
+    
+    def refine_mask_(self, mask_index):
+        self.mask1d = (self.mask1d.byte() & (self.data1d != mask_index)).float()
+        self.mask2d = (self.mask2d.byte() & (self.data2d != mask_index)).float()
 
 
 class CharWordBlock(nn.Module):
@@ -50,56 +86,39 @@ class CharWordBlock(nn.Module):
         self.cross = cross
         self.causal = causal
 
-    def forward(self, x2d, m2d, idx1d, idx2d, max_char, encoding=None, mask_src=None, reshape_back=True):
 
+    def forward(self, message, enc=None, enc_mask=None):
+
+        batch_size, max_num_word, max_size_word, max_num_char = \
+        message.batch_size, message.max_num_word, message.max_size_word, message.max_num_char
+
+        # ASSUME our inputs are synced #
+    
         # ================= self char-attention ================================== #
-        batch_size, max_num_word, max_size_word, d_model = x2d.size()
-        x2d = x2d.view(-1, max_size_word, d_model)
-        x2d = self.selfcharattn(x2d, x2d, x2d, m2d.view(-1, max_size_word)).view(batch_size, max_num_word, max_size_word, d_model) 
+        x2d = message.data2d.view(batch_size * max_num_word, max_size_word, -1)
+        m2d = message.mask2d.view(batch_size * max_num_word, max_size_word)
+        message.data2d = self.selfcharattn(x2d, x2d, x2d, m2d).view(batch_size,max_num_word, max_size_word, -1) 
+        message.to1d_()
 
         # ================= self word-attention ================================= #
 
-        # -- prepare query --
-        x1d = x2d.new_zeros(batch_size * max_char, d_model)
-        x1d[idx1d] = x2d.view(-1, d_model)[idx2d]
-        x1d = x1d.view(batch_size, -1, d_model).contiguous()  # batchsize x max_char x d_model
-
         # -- prepare keys & masks --
-        x1d_words = x2d[:, :, -1]  # batchsize x max_num_word x d_model
-        m1d_words = m2d[:, :, -1]  # batchsize x max_num_word
+        x1d_words = message.data2d[:, :, -1]  # batchsize x max_num_word x d_model
+        m1d_words = message.mask2d[:, :, -1]  # batchsize x max_num_word
 
         if self.causal:  # build special masks for causal word attention!!
-            
-            tri_mask = 1 - m1d_words.new_ones(max_num_word, max_num_word).triu(1)   # max_num_word x max_num_word
-            tri_mask = tri_mask[None, :, None, :].expand(batch_size, max_num_word, max_size_word, max_num_word).contiguous().view(-1, max_num_word)  # (max_num_word  x max_size_word) x max_num_word
-            new_mask = tri_mask.new_zeros(batch_size * max_char, max_num_word)
-            new_mask[idx1d] = tri_mask[idx2d]
-            m1d_words = new_mask.view(batch_size, max_char, max_num_word) * m1d_words[:, None, :].expand(batch_size, max_char, max_num_word)  # () x max_num_word
-
-        # print(x1d.size(), x1d_words.size(), m1d_words.size())
-        x1d = self.selfwordattn(x1d, x1d_words, x1d_words, m1d_words)
+            m1d_words = message.causal_mask * m1d_words[:, None, :].expand(batch_size, max_num_char, max_num_word)  # () x max_num_word
+        message.data1d = self.selfwordattn(message.data1d, x1d_words, x1d_words, m1d_words)
 
         # ================== cross word-attention =============================== #
-        
         if self.cross:   # only works when doing decoding.
-            encoding = encoding[:, :, -1]
-            mask_src = mask_src[:, :, -1]
-            x1d = self.crosswordattn(x1d, encoding, encoding, mask_src)
+            message.data1d = self.crosswordattn(message.data1d, enc[:, :, -1], enc[:, :, -1], enc_mask[:, :, -1])
 
         # ================== feed-forward ======================================= #
-        x1d = self.feedforward(x1d)
+        message.data1d = self.feedforward(message.data1d)
+        message.to2d_()
 
-        # ================== return to 2D ======================================= #
-        if reshape_back:
-            x2d = x1d.new_zeros(batch_size * max_num_word * max_size_word, d_model)
-            x2d[idx2d] = x1d.view(-1, d_model)[idx1d]
-            x2d = x2d.view(batch_size, max_num_word, max_size_word, d_model).contiguous()
-
-            return x2d
-
-        else:
-            
-            return x1d  
+        return message
 
 
 class EncoderC(Encoder):
@@ -119,14 +138,15 @@ class EncoderC(Encoder):
         self.share_embeddings = args.share_embeddings
         self.normalize_emb = args.normalize_emb
 
-    def forward(self, x, mask, idx1d, idx2d, max_char):
+    def forward(self, message):
 
-        encoding = [x]
-        x = self.prepare_embedding(x)
-        
+        encoding = [message.data2d]
+        message.data1d = self.prepare_embedding(message.data1d)
+        message.to2d_()
+
         for layer in self.layers:
-            x = layer(x, mask, idx1d, idx2d, max_char)
-            encoding.append(x)
+            message = layer(message)
+            encoding.append(message.data2d)
 
         return encoding
 
@@ -149,21 +169,15 @@ class DecoderC(Decoder):
         self.cross_attn_fashion = args.cross_attn_fashion
         self.normalize_emb = args.normalize_emb
 
-    def forward(self, x, mask_trg, idx1d, idx2d, max_char, encoding=None, mask_src=None):
-        x = self.dropout(x)
-        if self.normalize_emb:
-            x = self.layernorm(x)
+    def forward(self, message, all_enc=None, enc_mask=None):
+        message.data1d = self.prepare_embedding(message.data1d)
+        message.to2d_()
             
-        encoding = self.prepare_encoder(encoding)
-        L = len(self.layers)
-        for l, (layer, enc) in enumerate(zip(self.layers, encoding)):
-            if l < (L - 1):
-                x = layer(x, mask_trg, idx1d, idx2d, max_char, enc, mask_src)
-            else:
-                # Important: we do not need to reshape back from last decoder layer
-                x = layer(x, mask_trg, idx1d, idx2d, max_char, enc, mask_src, reshape_back=False)
+        all_enc = self.prepare_encoder(all_enc)
+        for l, (layer, enc) in enumerate(zip(self.layers, all_enc)):
+            message = layer(message, enc, enc_mask)
 
-        return x
+        return message
 
 
 
@@ -187,27 +201,14 @@ class TransformerC(Transformer):
         # decode or not:
         self.decode = False
 
-    def prepare_masks(self, inputs, symbol='<pad>', masks=None):
-        """
-        CAUTION: this function is working differently as in Transformer
-        """
-        field, text = inputs
-        
-        if masks is None:
-            masks = ((text.data != self.fields[field].vocab.stoi[symbol])).float()
-        else:
-            masks = (masks.byte() & (text.data != self.fields[field].vocab.stoi[symbol])).float()
-        return masks
+    def prepare_masks(self, field, text=None, symbol='<pad>'):
+        return (text != self.fields[field].vocab.stoi[symbol]).float()
 
-    def trans_2d_to_1d(self, data2d, mask2d):
+    def get_messages(self, field, data2d, mask2d, causal=False):
 
         # get sizes
         batch_size, max_num_word, max_size_word = data2d.size()
         max_num_char = mask2d.view(batch_size, -1).sum(1).long().max().item()
-
-        # everything put in 1D
-        data1d = data2d.new_ones(batch_size * max_num_char)
-        mask1d = mask2d.new_zeros(batch_size * max_num_char)
 
         # source / target pairing
         idx2d = mask2d.view(-1).nonzero().squeeze(1)
@@ -218,43 +219,55 @@ class TransformerC(Transformer):
         idx1d = torch.arange(idx2d.size(0), device=idx2d.get_device()) - _temp2[_temp1] + _temp1 * max_num_char
 
         # assign data to 1D
+        data1d = data2d.new_ones(batch_size * max_num_char)
+        mask1d = mask2d.new_zeros(batch_size * max_num_char)
+        
         data1d[idx1d] = data2d.view(-1)[idx2d]
         mask1d[idx1d] = mask2d.view(-1)[idx2d]
-        return data1d.view(batch_size, max_num_char).contiguous(), mask1d.view(batch_size, max_num_char), idx2d, idx1d, max_num_char      
+        
+        data1d = data1d.view(batch_size, max_num_char)
+        mask1d = mask1d.view(batch_size, max_num_char)
+
+        # prepare a casual mask
+        if causal:
+            tri_mask = 1 - mask1d.new_ones(max_num_word, max_num_word).triu(1)   # max_num_word x max_num_word
+            tri_mask = tri_mask[None, :, None, :].expand(batch_size, max_num_word, max_size_word, max_num_word).contiguous().view(-1, max_num_word)  # (max_num_word  x max_size_word) x max_num_word
+            new_mask = tri_mask.new_zeros(batch_size * max_num_char, max_num_word)
+            new_mask[idx1d] = tri_mask[idx2d]
+            new_mask = new_mask.view(batch_size, max_num_char, max_num_word)    # () x max_num_word
+        
+        else:
+            new_mask = None
+
+        # output message
+        message = Message(data1d, mask1d, data2d, mask2d, idx1d, idx2d, batch_size, max_num_char, max_size_word, max_num_word, new_mask)
+        message.refine_mask_(self.fields[field].vocab.stoi['<eos>'])  # mask out <EOS> in the input sequence
+        return message
 
     def prepare_data(self, batch):
         source_inputs, target_inputs = batch.src, batch.trg
-        source_masks, target_masks = self.prepare_masks(('src', source_inputs)), self.prepare_masks(('trg', target_inputs))
+        source_masks, target_masks = self.prepare_masks('src', source_inputs), self.prepare_masks('trg', target_inputs)
 
-        source_inputs1d, source_mask1d, source_idx2d, source_idx1d, source_max_char = self.trans_2d_to_1d(source_inputs, source_masks)
-        target_inputs1d, target_mask1d, target_idx2d, target_idx1d, target_max_char = self.trans_2d_to_1d(target_inputs, target_masks)
+        source_message = self.get_messages('src', source_inputs, source_masks)
+        target_message = self.get_messages('trg', target_inputs, target_masks, causal=True)
+        return source_message, target_message
 
-        # refine the masks (because we need to maskout <eos> in the input side.)
-        source_masks = self.prepare_masks(('src', source_inputs), '<eos>', source_masks)
-        target_masks = self.prepare_masks(('src', target_inputs), '<eos>', target_masks)
+    def encoding(self, message):
+        message.data1d = self.io_enc.i(message.data1d, pos=True)
+        message.to2d_()
+        return self.encoder(message)
 
-        source_outputs1d, source_mask1d = source_inputs1d[:, 1:], source_mask1d[:, 1:]
-        target_outputs1d, target_mask1d = target_inputs1d[:, 1:], target_mask1d[:, 1:]
-
-        return source_inputs, source_outputs1d, source_masks, source_mask1d, \
-            target_inputs, target_outputs1d, target_masks, target_mask1d, \
-            source_idx1d, source_idx2d, source_max_char, target_idx1d, target_idx2d, target_max_char
-
-    
-    def encoding(self, encoder_inputs, encoder_masks, idx1d, idx2d, max_char):
-        return self.encoder(self.io_enc.i(encoder_inputs, pos=True), encoder_masks, idx1d, idx2d, max_char)
-
-    def decoding(self, decoder_inputs, decoder_masks, idx1d, idx2d, max_char,
-                encoding_outputs, encoder_masks,
-                decoding=False, beam=1, alpha=0.6, return_probs=False):
+    def decoding(self, message, all_enc, enc_mask, decoding=False, beam=1, alpha=0.6, return_probs=False):
 
         if (return_probs and decoding) or (not decoding):
-            out = self.decoder(self.io_dec.i(decoder_inputs, pos=True), decoder_masks, idx1d, idx2d, max_char, encoding_outputs, encoder_masks)
+            message.data1d = self.io_dec.i(message.data1d, pos=True)
+            message.to2d_()
+            message = self.decoder(message, all_enc, enc_mask)
 
         if decoding:
             raise NotImplementedError
 
-        return out
+        return message.data1d
 
 
     def forward(self, batch, decoding=False, reverse=True):
@@ -262,37 +275,25 @@ class TransformerC(Transformer):
         #if info is None:
         info = defaultdict(lambda: 0)
 
-        source_inputs, source_outputs1d, source_masks, source_masks1d, \
-        target_inputs, target_outputs1d, target_masks, target_masks1d, \
-        source_idx1d, source_idx2d, source_max_char, \
-        target_idx1d, target_idx2d, target_max_char = self.prepare_data(batch)
+        source_message, target_message = self.prepare_data(batch)
+        target_outputs1d = target_message.data1d[:, 1:]
+        target_masks1d   = target_message.mask1d[:, :-1]
 
-        info['sents']  = (target_outputs1d[:, 0] * 0 + 1).sum()
-        info['tokens'] = (target_masks1d != 0).sum()
+        info['sents']  = (source_message.data1d[:, 0] * 0 + 1).sum()
+        info['tokens'] = (target_message.mask1d != 0).sum()
 
-        # # in some extreme case.
-        # if info['sents'] == 0:
-        #     return info
-
-        # encoding
-        encoding_outputs = self.encoding(source_inputs, source_masks, source_idx1d, source_idx2d, source_max_char)
-
+        encoding_outputs = self.encoding(source_message)
         if not decoding:
-
-            # Maximum Likelihood Training (with label smoothing trick)
-            decoding_outputs = self.decoding(target_inputs, target_masks, target_idx1d, target_idx2d, target_max_char, encoding_outputs, source_masks)[:, :-1]   
+            decoding_outputs = self.decoding(target_message, encoding_outputs, source_message.mask2d)[:, :-1]  
+            print(target_outputs1d.size(), target_masks1d.size(), decoding_outputs.size())
             loss = self.io_dec.cost(target_outputs1d, target_masks1d, outputs=decoding_outputs, label_smooth=self.args.label_smooth)
             
             for w in loss:
                 info['L@' + w] = loss[w]
                 if w[0] != '#':
                     info['loss'] = info['loss'] + loss[w]
-
-            
-
         else:
 
             raise NotImplementedError
-    
         
         return info
